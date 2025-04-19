1031098db1fbda37c621a18212e9558539b57773
CoolBeanGames <Coolbeangames@gmail.com>
2025-04-19 03:24:40 -0500

    attemting to fix the issues

--- addons/ThreeSix/Dialog CSV Importer/csvUI.gd
+++ addons/ThreeSix/Dialog CSV Importer/csvUI.gd
@@ -25,19 +25,16 @@ func get_sheet_number() -> int:
 
 ##update bottom text
 func update_changed_entries(text : String):
-	$"Main Container/HBoxContainer2/changed entries".text = "entries changed: " + text
+	$"Main Container/HBoxContainer2/changed entries".text = text
 
 func update_new_entries(text : String):
-	$"Main Container/HBoxContainer2/new entries".text = "new entries: " + text
+	$"Main Container/HBoxContainer2/new entries".text = text
 
 func update_entries_created(text : String):
-	$"Main Container/HBoxContainer2/entries created".text ="entries: " + text
+	$"Main Container/HBoxContainer2/entries created".text = text
 
-func update_time_taken(text : float):
-	if text > 1000:
-		$"Main Container/HBoxContainer2/time taken".text ="time taken: " + str(text/1000) + " seconds"
-		return
-	$"Main Container/HBoxContainer2/time taken".text ="time taken: " + str(text/1000) + " MS"
+func update_time_taken(text : String):
+	$"Main Container/HBoxContainer2/time taken".text = text
 
 func update_csv_preview(text : String):
 	$"Main Container/ScrollContainer/VBoxContainer/csv_preview/Panel/RichTextLabel".text = text
--- addons/ThreeSix/Dialog CSV Importer/savable_resource.gd
+++
@@ -1,6 +0,0 @@
-class_name savable_resource
-
-var resource_name : String = ""
-var resource : Resource
-var path : String = ""
-var is_new : bool = false
